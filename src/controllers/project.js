b8eacd166b32485bfe1dbda3013c16ae16ebebc0
shmohamud <shmohamud@email.wm.edu>
2020-12-11 15:19:43 -0500

    error handling, updateProject works

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -4,21 +4,37 @@ const Project = require("../models/project");
 
 const createProject = async function (project) {
   project._id = mongoose.Types.ObjectId();
-  await Project.create(project, async function (err) {
-    if (err) {
-      console.log("Error: ", err);
-    }
-    console.log("Successfully created");
-  });
+  try {
+    await Project.create(project).exec();
+  } catch (err) {
+    console.log("Error: ", err.stack);
+  }
 };
 
 const getProjects = async () => {
-  const projects = await Project.find({});
-  return projects;
+  try {
+    const projects = await Project.find({}).exec();
+    return projects;
+  } catch (err) {
+    console.log(err.stack);
+  }
 };
 
 const getProject = async (id) => {
-  const project = await Project.findById(id);
+  const project = await Project.findById(id).exec();
+  return project;
+};
+
+const updateProject = async (id, update) => {
+  const filter = { _id: id };
+  try {
+    const project = await Project.findOneAndUpdate(filter, update, {
+      new: true,
+    }).exec();
+    return project;
+  } catch (err) {
+    console.log("Error: ", err, stack);
+  }
   return project;
 };
 
@@ -26,4 +42,5 @@ module.exports = {
   createProject: createProject,
   getProjects: getProjects,
   getProject: getProject,
+  updateProject: updateProject,
 };
