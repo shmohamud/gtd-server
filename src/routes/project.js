31ec047f7c7a7ef222cd53b86e868d8afc44fb71
shmohamud <shmohamud@email.wm.edu>
2020-12-16 20:48:43 -0500

    project POST route fix

--- src/routes/project.js
+++ src/routes/project.js
@@ -5,7 +5,7 @@ const projects = express.Router();
 
 projects.get("/", async (req, res) => Project.all(req,res))
 projects.get("/:project_id", async (req, res) => Project.byId(req, res))
-projects.post("/create", async (req, res) => Project.create(req, res));
+projects.post("/", async (req, res) => Project.create(req, res));
 projects.post("/:project_id/actions", async (req, res) => Action.create(req, res));
 projects.patch("/:project_id", async (req, res) => Project.update(req, res))
 projects.delete("/:project_id", async (req, res) => Project.destroy(req, res))
