c20846e75f626515444f15122468b2b9ac3b807e
shmohamud <shmohamud@email.wm.edu>
2021-02-10 03:08:28 -0500

    add response headers

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,13 @@ const app = new express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');  
+  next()
+})
 app.use("/projects", projects);
 app.use("/actions", actions);
 app.use("/braindumps", braindumps);
